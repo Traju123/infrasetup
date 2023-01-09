133d0f6fb2b25b64f5211aa0c20be2aa5e5ab7db
Traju123 <108616014+Traju123@users.noreply.github.com>
2023-01-09 15:06:06 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -32,8 +32,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
 output "client_certificate" {
   value = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
+  sensitive = true
 }
 
 output "kube_config" {
   value = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
 }
