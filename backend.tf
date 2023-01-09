8020ab4b3362a762b3ae7aed179ffad9b8395c38
Traju123 <108616014+Traju123@users.noreply.github.com>
2023-01-09 15:04:48 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -20,11 +20,11 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = var.default_node_pool_vm_size
+    vm_size    = "Standard_D2_v2"
   }
 
   identity {
-    type = "Standard_D2_v2"
+    type = "SystemAssigned"
 
   }
 
