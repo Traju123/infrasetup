1eb40a47ed675bea594a43e6d22ea4d35695f081
Traju123 <108616014+Traju123@users.noreply.github.com>
2023-01-09 15:01:37 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -15,3 +15,30 @@ resource "azurerm_resource_group" "state-demo-secure" {
   name     = "state-demo"
   location = "eastus"
 }
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "demo1"
+  location            = "eastus"
+  resource_group_name = "testresourse3112"
+  dns_prefix          = "test"
+
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = var.default_node_pool_vm_size
+  }
+
+  identity {
+    type = "Standard_D2_v2"
+
+  }
+
+}
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+}
